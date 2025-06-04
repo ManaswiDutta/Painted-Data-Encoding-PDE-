9d07b0f33a1285dc31d8a4005d93f52b0e4b7fe8
Manaswi Dutta <manaswidutta2025@gmail.com>
2025-06-04 15:24:10 +0530

    Update msp_decoder.py

--- msp_decoder.py
+++ msp_decoder.py
@@ -1,22 +1,36 @@
-from PIL import Image, ImageDraw
-letters=" ABCDEFGHIJKLMNOPQRSTUVWXYZ.,?abcdefghijklmnopqrstuvwxyz"
-data = ""
-img = Image.open("trial2.png")
-x,y=0,0
-while img.getpixel((x,y)) != (255, 255, 255):
-    r, g, b = img.getpixel((x,y))
-    dec = (r * 65536) + (g * 256) + b
-    a = dec // 262144
-    dec = dec % 262144
-    b = dec // 4096
-    dec = dec % 4096
-    c = dec // 64
-    d = dec % 64
-    data += letters[a] + letters[b] + letters[c] + letters[d]
-    if y==144:
-        x+=1
-        y=0
-    else:
-        y+=1
+from PIL import Image
 
-print(data)
+letters = " ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz.,?!@#"
+# assert len(letters) == 64  # critical
+
+img = Image.open("trial2.png")
+width, height = img.size
+
+data = ""
+x, y = 0, 0
+
+while x < width:
+    pixel = img.getpixel((x, y))
+
+    # Stop if white pixel (end marker)
+    if pixel == (255, 255, 255):
+        break
+
+    r, g, b = pixel
+    dec = (r * 65536) + (g * 256) + b
+
+    a = (dec // 262144) % 64
+    dec = dec % 262144
+    b = (dec // 4096) % 64
+    dec = dec % 4096
+    c = (dec // 64) % 64
+    d = dec % 64
+
+    data += letters[a] + letters[b] + letters[c] + letters[d]
+
+    y += 1
+    if y >= height:
+        y = 0
+        x += 1
+
+print("The Data is :  "+data.strip())
